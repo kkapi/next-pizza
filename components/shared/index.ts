115d97bb927fdabd4e159e79cbfd1c57d2262c9a
kkapi <hZG2fik_j8@mail.ru>
2024-10-02 00:22:25 +0300

    refactor: move components to index.ts

---
+++ components/shared/index.ts
@@ -0,0 +1,8 @@
+export { Container } from './container';
+export { Header } from './header';
+export { Categories } from './categories';
+export { TopBar } from './top-bar';
+export { SortPopup } from './sort-popup';
+export { Filters } from './filters';
+export { FilterCheckbox } from './filter-checkbox';
+export { CheckboxFilterGroup } from './checkbox-filter-group';
